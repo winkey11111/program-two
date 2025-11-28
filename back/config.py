695a91b1349ef8f6aabafb256020ce9e52962e72
winkey11111 <2378174283@qq.com>
2025-11-28 08:45:51 +0800

    wyq

--- back/config.py
+++ back/config.py
@@ -11,14 +11,14 @@ RESULT_DIR = os.path.join(BASE_DIR, "static", "results")
 CAMERA_DIR = os.path.join(BASE_DIR, "static", "camera_records")
 MODEL_PATH = os.path.join(BASE_DIR, "yolov8", "best.pt")
 
-# 自动创建目录（如果不存在）
+# 自动创建目录
 for directory in [UPLOAD_DIR, RESULT_DIR, CAMERA_DIR]:
     os.makedirs(directory, exist_ok=True)
 
 # -------------------------------
-# 数据库配置（改为 SQLite）
+# 数据库配置
 # -------------------------------
-# SQLite 数据库存放路径：与项目同级的 db 目录下
+# SQLite 数据库存放路径
 DB_DIR = os.path.join(BASE_DIR, "db")
 os.makedirs(DB_DIR, exist_ok=True)  # 确保 db 目录存在
 
